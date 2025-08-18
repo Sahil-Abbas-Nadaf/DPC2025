ae62a4322eba9f79ca932e2485a5fafee8a9240a
Sahil Abbas Nadaf <nadafsahil2005@gmail.com>
2025-08-18 23:09:22 +0530

    Add files via upload
    Day 4 Challenge Completed

---
+++ Day4.java
@@ -0,0 +1,47 @@
+package DialyProgrammingChallenge;
+
+import java.util.*;
+
+public class Day4 {
+    static void merge(int[] arr1, int[] arr2, int m, int n) {
+        int gap = (m + n + 1) / 2;
+        while (gap > 0) {
+            int i = 0;
+            int j = gap;
+            while (j < m + n) {
+                if (i < m && j < m) {
+                    if (arr1[i] > arr1[j]) {
+                        int temp = arr1[i];
+                        arr1[i] = arr1[j];
+                        arr1[j] = temp;
+                    }
+                } else if (i < m && j >= m) {
+                    if (arr1[i] > arr2[j - m]) {
+                        int temp = arr1[i];
+                        arr1[i] = arr2[j - m];
+                        arr2[j - m] = temp;
+                    }
+                } else {
+                    if (arr2[i - m] > arr2[j - m]) {
+                        int temp = arr2[i - m];
+                        arr2[i - m] = arr2[j - m];
+                        arr2[j - m] = temp;
+                    }
+                }
+                i++;
+                j++;
+            }
+            if (gap == 1) gap = 0;
+            else gap = (gap + 1) / 2;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 3, 5, 7};
+        int[] arr2 = {2, 4, 6, 8};
+        merge(arr1, arr2, arr1.length, arr2.length);
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+    }
+}
+

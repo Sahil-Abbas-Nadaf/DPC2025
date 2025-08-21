7d64760b8902e2c5793ad6c133ebe698586be52d
Sahil Abbas Nadaf <nadafsahil2005@gmail.com>
2025-08-21 22:32:33 +0530

    Add files via upload
    Day 7 Challenge Completed

---
+++ Day7.java
@@ -0,0 +1,48 @@
+package DialyProgrammingChallenge;
+
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        if (n < 3) return 0;
+
+        int left = 0, right = n - 1;
+        int leftMax = 0, rightMax = 0;
+        int water = 0;
+
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    water += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    water += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+
+        return water;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] h1 = {0,1,0,2,1,0,1,3,2,1,2,1};
+        int[] h2 = {4,2,0,3,2,5};
+        int[] h3 = {1,1,1};
+        int[] h4 = {5};
+        int[] h5 = {2,0,2};
+
+        System.out.println(sol.trap(h1)); // 6
+        System.out.println(sol.trap(h2)); // 9
+        System.out.println(sol.trap(h3)); // 0
+        System.out.println(sol.trap(h4)); // 0
+        System.out.println(sol.trap(h5)); // 2
+    }
+}
+

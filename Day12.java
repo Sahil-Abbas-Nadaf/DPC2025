4b8ff3acf8fbaa96b0fc0a5bdd2cbca6b8cc61dd
Sahil Abbas Nadaf <nadafsahil2005@gmail.com>
2025-08-26 20:54:56 +0530

    Add files via upload
    Day 12 Challenge Completed

---
+++ Day12.java
@@ -0,0 +1,33 @@
+package DialyProgrammingChallenge;
+
+import java.util.Stack;
+
+public class Day12 {
+    public static void main(String[] args) {
+        System.out.println(isValid("()"));
+        System.out.println(isValid("([)]"));
+        System.out.println(isValid("[{()}]"));
+        System.out.println(isValid(""));
+        System.out.println(isValid("{[}"));
+    }
+
+    static boolean isValid(String s){
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch == '{' || ch == '['){
+                stack.push(ch);
+            }else{
+                if(stack.isEmpty()) return false;
+                char top = stack.pop();
+
+                if((ch == ')' && top != '(') ||
+                   (ch == '}' && top != '{') ||
+                   (ch == ']' && top != '[')){
+                    return false;
+                }
+             }
+        }
+        return stack.isEmpty();
+    }
+}
